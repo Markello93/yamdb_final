09cef3eeca136ed834c436430de3c0e0022e7aaa
Марк Крылов <mark.krylov93@gmail.com>
2023-02-16 20:49:58 +0600

    isort

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,15 @@
+from api_yamdb.api.filters import FilterForTitle
 from api.mixins import CreateListDestroyViewSet
 from api.permissions import AdminModeratorAuthorOrReadOnly, AdminOrReaOnly
 from api.serializers import (CategorySerializer, CommentSerializer,
                              GenreSerializer, ReviewSerializer,
                              TitleCreateSerializer, TitleReadSerializer)
-from api_yamdb.api.filters import FilterForTitle
+from reviews.models import Category, Genre, Review, Title
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-
-from reviews.models import Category, Genre, Review, Title
 
 
 class CategoryViewSet(CreateListDestroyViewSet):
