23875a0b8724439588d081b10ccc9d5f6aed7d62
Марк Крылов <mark.krylov93@gmail.com>
2023-02-16 21:16:36 +0600

    isort

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,8 +4,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-dotenv_path = Path('infra/.env')
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
